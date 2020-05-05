7ba1c8f9d01e9071c71544e0853b9974adc6f15c
ahsankhan1911 <ahsankhan1911@gmail.com>
2020-05-06 02:26:55 +0500

    new build added

---
+++ client/build/precache-manifest.7030c06715df31df86f10f7e3f70c5fb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "f65c7dc36224cf175d91",
+    "url": "/static/js/main.f65c7dc3.chunk.js"
+  },
+  {
+    "revision": "295cc054470fa1406210",
+    "url": "/static/js/2.295cc054.chunk.js"
+  },
+  {
+    "revision": "f65c7dc36224cf175d91",
+    "url": "/static/css/main.ecbd9bef.chunk.css"
+  },
+  {
+    "revision": "2b7416cd81b153813204ba5699ef99ea",
+    "url": "/index.html"
+  }
+];
